d3b352858d82a6330f461b693685574df588bcb6
Ramon <rcalero@insalmenar.cat>
2026-05-19 00:58:31 +0200

    video9 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -6,3 +6,15 @@ function obtenerDatosHtml(nombre) {
 
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
+function obtenerContactos() {
+
+    let hoja = SpreadsheetApp.openById('1r55A61lVoehpus_gCA816qCOdsRo3k2_045fTbgeu4s').getSheets()[0];
+
+    let datos = hoja.getDataRange().getDisplayValues();
+
+    // 4. Fem un Logger per veure des de la web de Google si realment llegeix alguna cosa
+  Logger.log("Dades llegides del Sheets: " + datos.length + " files trobades.");
+
+    return datos;
+}
