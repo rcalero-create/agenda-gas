948c0c7676187e3cf6fe23fe9b10402e75d4b5c7
Ramon <rcalero@insalmenar.cat>
2026-05-20 21:12:19 +0200

    Video15 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -1,20 +1,25 @@
+const HOJA = SpreadsheetApp.openById('1r55A61lVoehpus_gCA816qCOdsRo3k2_045fTbgeu4s').getActiveSheet();
+
 function doGet() {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
 }
 
-function obtenerDatosHtml(nombre) {
+function doPost(e) {
+    
+    insertarContacto(e.parameter.nombre, e.parameter.correo);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+}
 
+function obtenerDatosHtml(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
 function obtenerContactos() {
+  
+    return HOJA.getDataRange().getDisplayValues();
+  
+}
 
-    let hoja = SpreadsheetApp.openById('1r55A61lVoehpus_gCA816qCOdsRo3k2_045fTbgeu4s').getSheets()[0];
-
-    let datos = hoja.getDataRange().getDisplayValues();
-
-    // 4. Fem un Logger per veure des de la web de Google si realment llegeix alguna cosa
-  Logger.log("Dades llegides del Sheets: " + datos.length + " files trobades.");
-
-    return datos;
+function insertarContacto(nombre, correo) {
+  HOJA.appendRow([nombre, correo]);
 }
