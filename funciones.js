52287020a4248b30281703d3b6e86a8af7c2aec1
Ramon <rcalero@insalmenar.cat>
2026-05-20 12:16:50 +0200

    Pujada definitiva dels fitxers modificats amb la v4

--- funciones.js
+++ funciones.js
@@ -1,5 +1,14 @@
+const HOJA = hoja = SpreadsheetApp.openById('1r55A61lVoehpus_gCA816qCOdsRo3k2_045fTbgeu4s').getActiveSheet();
+
 function doGet() {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+}
+
+function doPost(datos) {
+
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo);
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google Apps Script');
+
 }
 
 function obtenerDatosHtml(nombre) {
@@ -9,12 +18,15 @@ function obtenerDatosHtml(nombre) {
 
 function obtenerContactos() {
 
-    let hoja = SpreadsheetApp.openById('1r55A61lVoehpus_gCA816qCOdsRo3k2_045fTbgeu4s').getSheets()[0];
+    
 
-    let datos = hoja.getDataRange().getDisplayValues();
+    return HOJA.getDataRange().getDisplayValues();
 
     // 4. Fem un Logger per veure des de la web de Google si realment llegeix alguna cosa
   Logger.log("Dades llegides del Sheets: " + datos.length + " files trobades.");
 
-    return datos;
 }
+
+function insertarContacto(nombre, correo) {
+  HOJA.appendRow([nombre, correo]);
+} 
