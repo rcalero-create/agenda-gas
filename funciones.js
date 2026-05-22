0acfe9ce3eb1752886475f8ffdcbbd250179a3de
Ramon <rcalero@insalmenar.cat>
2026-05-22 13:13:29 +0200

    Video20 curso gas-avanzado

--- funciones.js
+++ funciones.js
@@ -22,8 +22,7 @@ function obtenerContactos() {
 function insertarContacto(nombre, apellidos, correo, telefono) {
   HOJA.appendRow([nombre, apellidos, correo, telefono]);
 }
-<<<<<<< HEAD
-=======
+
 
 function borrarContacto(filaIndex) {
   HOJA.deleteRow(filaIndex);
@@ -34,4 +33,3 @@ function modificarContacto(numFila, datos) {
   let celdas = HOJA.getRange('A' + numFila + ':D' + numFila);
   celdas.setValues([[datos.nombre, datos.apellidos, datos.correo, datos.telefono]]);
 }
->>>>>>> 5f35a1bad1e345bf291337a4e0f824925b82565d
